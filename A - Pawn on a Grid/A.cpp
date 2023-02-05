1ab6bbe6ffc4864e864e1c419fc2d8ff326e149d
glaucogithub <75082173+glaucogithub@users.noreply.github.com>
2023-02-05 00:14:25 -0400

    Upload Atcoder

---
+++ A - Pawn on a Grid/A.cpp
@@ -0,0 +1,78 @@
+
+// author: glaucoacassioc
+// created on: 05/02/2023 às 00:09:52
+
+// Problem: A - Pawn on a Grid
+// URL: https://atcoder.jp/contests/abc280/tasks/abc280_a
+// Time Limit: 2000 ms
+// Memory Limit: 1024 MB 
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+ 
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+	Fun fun_;
+public:
+	template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+	template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+ 
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template < typename T_container, typename T = typename enable_if < !is_same<T_container, string>::value, typename T_container::value_type >::type > ostream & operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef DEBUG
+#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+ 
+ 
+void run_case() {
+	int h, w, ans = 0;
+	cin >> h >> w;
+	vector<vector<char> > grid(h, vector<char>(w));
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < w; ++j) {
+			cin >> grid[i][j];
+			if(grid[i][j] == '#') {
+				ans++;
+			}
+		}
+	}
+	cout << ans << '\n';
+}
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+#ifdef DEBUG
+	freopen("sample1.in", "r", stdin);
+#endif
+	int T = 1;
+	// cin >> T;
+	while (T-- > 0) {
+		run_case();
+	}
+}
+ 
+ 
